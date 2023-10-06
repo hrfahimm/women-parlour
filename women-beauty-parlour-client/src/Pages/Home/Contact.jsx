a58ce05e83d91bd10389e3e4b2ce95e8df104dbf
hrfahimm <fahimtaj10@gmail.com>
2023-10-06 21:08:53 +0600

    'd'

--- women-beauty-parlour-client/src/Pages/Home/Contact.jsx
+++ women-beauty-parlour-client/src/Pages/Home/Contact.jsx
@@ -6,18 +6,67 @@ const Contact = () => {
     const { register, handleSubmit } = useForm();
     const onSubmit = (data) => console.log(data);
     return (
-        <div>
-            {" "}
-            <form onSubmit={handleSubmit(onSubmit)}>
-                <input />
-                <input
-                    {...register("firstName")}
-                    type='text'
-                    placeholder='Type here'
-                    className='input input-bordered w-full max-w-xs'
-                />
-                <input {...register("lastName")} />
-                <input type='submit' />
+        <div className='bg-pink-50 p-4 py-14'>
+            <h1 className='text-center text-3xl font-bold pb-12'>
+                Lets Us Handle Your <br />
+                Projects Profationally{" "}
+            </h1>{" "}
+            <form
+                onSubmit={handleSubmit(onSubmit)}
+                className='card-body'>
+                <div className='form-control grid grid-cols-2 gap-4'>
+                    <input
+                        type='text'
+                        name='name'
+                        {...register("name", { required: true })}
+                        placeholder='Fast Name'
+                        className='input input-bordered'
+                    />
+                    <input
+                        placeholder='Sure name'
+                        type='text'
+                        {...register("sureName", {
+                            required: true,
+                        })}
+                        className='input input-bordered'
+                    />
+                </div>
+
+                <div className='form-control grid grid-cols-2 gap-4'>
+                    <input
+                        type='text'
+                        name='name'
+                        {...register("name", { required: true })}
+                        placeholder='Email'
+                        className='input input-bordered'
+                    />
+                    <input
+                        placeholder='Phone Number'
+                        type='text'
+                        {...register("sureName", {
+                            required: true,
+                        })}
+                        className='input input-bordered'
+                    />
+                </div>
+
+                <div className='form-control'>
+                    <input
+                        type='email'
+                        name='email'
+                        {...register("Phonenumber", { required: true })}
+                        placeholder='Phonenumber'
+                        className='input input-bordered'
+                    />
+                </div>
+
+                <div className='form-control m-2 text-center items-center  '>
+                    <input
+                        className='btn bg-[#F63E7B] max-w-xs text-center'
+                        type='submit'
+                        value='SignUp'
+                    />
+                </div>
             </form>
         </div>
     );
--- women-beauty-parlour-client/src/Pages/Home/Testimonial.jsx
+++ women-beauty-parlour-client/src/Pages/Home/Testimonial.jsx
@@ -4,7 +4,7 @@ import img2 from "../../assets/img2.png";
 const testimonial = () => {
     return (
         <div>
-            <div className='hero min-h-[677px] p-8 '>
+            <div className='hero min-h-[677px] p-8 bg-pink-50'>
                 <div className='hero-content flex-col lg:flex-row'>
                     <img
                         src={img2}
@@ -28,7 +28,7 @@ const testimonial = () => {
                                     500+
                                 </h1>
                                 <h1> Happy Customer</h1>
-                            </div> 
+                            </div>
                             <div className='text-center'>
                                 <h1 className=' text-[#F63E7B] text-2xl py-2  font-extrabold'>
                                     16+
